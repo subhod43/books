0a00e5316cb9ea732e8f83f5615ceef3aea2940f
Subhadeep Chakraborty <iamsubhad43@gmail.com>
2024-01-03 23:04:10 +0530

    demo app to create list of books with create, edit and delete features

---
+++ src/components/BookShow.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import BookEdit from "./BookEdit";
+
+function BookShow({ book, onEdit, onDelete }) {
+    const [showEdit, setShowEdit] = useState(false);
+
+    const handleDelete = () => {
+        onDelete(book.id);
+    }
+
+    const handleEdit = () => {
+        setShowEdit(!showEdit);
+    }
+
+    const handleSubmit = (id, newTitle) => {
+        setShowEdit(false);
+        onEdit(id, newTitle);
+    }
+
+    let content = <h3>{book.title}</h3>
+    if(showEdit) {
+        content = <BookEdit book={book} onSubmit={handleSubmit} />
+    }
+
+    return (
+        <div className="book-show">
+        <img alt="books" src={`https://picsum.photos/seed/${book.id}/300/200`} />
+            <div>{content}</div>
+            <div className="actions">
+                <button className="edit" onClick={handleEdit}>Edit</button>
+                <button className="delete" onClick={handleDelete}>Delete</button>
+            </div>
+        </div>
+    );
+}
+
+export default BookShow;
